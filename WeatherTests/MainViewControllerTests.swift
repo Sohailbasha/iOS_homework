60d62434147917df304275e0633d80b00996c1f8
Sohailbasha <shlbsh94@gmail.com>
2018-07-16 21:13:32 -0400

    removed default test code, with the exception of teardown and setup

---
+++ WeatherTests/MainViewControllerTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+class MainViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
