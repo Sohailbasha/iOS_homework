734884273b3e901b9209572e2a6cfe7c90438cbd
Sohailbasha <shlbsh94@gmail.com>
2018-07-24 12:17:03 -0400

    refactor code

--- Weather/View Controllers/LocationTableViewController.swift
+++ Weather/View Controllers/LocationTableViewController.swift
@@ -2,8 +2,27 @@ import UIKit
 import CoreData
 import CoreLocation
 
-class LocationTableViewController: UIViewController, NSFetchedResultsControllerDelegate, UINavigationBarDelegate, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+class LocationTableViewController: UIViewController, NSFetchedResultsControllerDelegate, CLLocationManagerDelegate {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableview()
+        setupNavBarAndBarButtons()
+        
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Error starting fetched results controller: \(error)")
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        tableView.frame = self.view.frame
+        tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+    }
+    
     var delegate: LocationSelectDelegate?
     var locationManager: CLLocationManager!
     
@@ -33,47 +52,52 @@ class LocationTableViewController: UIViewController, NSFetchedResultsControllerD
         let fetchRequest: NSFetchRequest<Location> = Location.fetchRequest()
         let sortDescriptor = NSSortDescriptor(key: "isCurrentLocation", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
-        return NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.context, sectionNameKeyPath: nil, cacheName: nil)
+        return NSFetchedResultsController(fetchRequest: fetchRequest,
+                                          managedObjectContext: CoreDataStack.context,
+                                          sectionNameKeyPath: nil,
+                                          cacheName: nil)
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @objc func showAddLocationsAlert() {
+        Alert.showAddLocationAlert(in: self)
+    }
+    
+    @objc func getCurrentLocation() {
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func getPlacemark(location: Location, completion: @escaping(_ location: String) -> ()) {
+        LocationGeocoder.geolocate(location: location) { (placemark, error) in
+            guard let city = placemark?.locality else { return }
+            guard let state = placemark?.administrativeArea else { return }
+            let location = "\(city), \(state)"
+            completion(location)
+        }
+    }
+    
+    func setupNavBarAndBarButtons() {
+        self.title = "Locations"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "search"), style: .plain, target: self, action: #selector(showAddLocationsAlert))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "marker"), style: .plain, target: self, action: #selector(getCurrentLocation))
+    }
+    
+    func setupTableview() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "locationCell")
         self.view.addSubview(tableView)
-        self.title = "Locations"
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissView))
-        
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showAddLocationsAlert))
+    }
 
-        fetchedResultsController.delegate = self
-        do {
-            try fetchedResultsController.performFetch()
-        } catch {
-            print("Error starting fetched results controller: \(error)")
-        }
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        tableView.frame = self.view.frame
-        tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
-    }
-    
-    
-    
-    @objc func dismissView() {
-        if let _ = fetchedResultsController.fetchedObjects?.isEmpty {
-            return
-        } else {
-            dismiss(animated: true, completion: nil)
-        }
-    }
-    
-    @objc func showAddLocationsAlert() {
-        self.showBasicLocationAlert(in: self, with: "Add a new location")
-    }
+}
+
+// MARK: - Table View Datasource / Delegate
+extension LocationTableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fetchedResultsController.fetchedObjects?.count ?? 0
@@ -81,7 +105,9 @@ class LocationTableViewController: UIViewController, NSFetchedResultsControllerD
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
-        guard let location = fetchedResultsController.fetchedObjects?[indexPath.row] else { return UITableViewCell() }
+        guard let location = fetchedResultsController.fetchedObjects?[indexPath.row] else {
+            return UITableViewCell()
+        }
         getPlacemark(location: location) { (city) in
             cell.textLabel?.text = city
         }
@@ -102,18 +128,12 @@ class LocationTableViewController: UIViewController, NSFetchedResultsControllerD
         }
     }
     
-    func getPlacemark(location: Location, completion: @escaping(_ location: String) -> ()) {
-        LocationGeocoder.geolocate(location: location) { (placemark, error) in
-            guard let city = placemark?.locality else { return }
-            guard let state = placemark?.administrativeArea else { return }
-            let location = "\(city), \(state)"
-            completion(location)
-        }
-    }
-
 }
 
 // MARK: - NSFetchedResultsControllerDelegate functions
+/*
+ 
+ */
 extension LocationTableViewController {
     
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
@@ -153,57 +173,8 @@ extension LocationTableViewController {
     }
 }
 
-extension LocationTableViewController: UIBarPositioningDelegate {
-    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
-        return .topAttached
-    }
-}
-
+// MARK: - Location Delegate Methods
 extension LocationTableViewController {
-    
-    func showBasicLocationAlert(in vc: UIViewController, with title: String, message: String? = nil) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        var tf: UITextField?
-        
-        alertController.addTextField { (textField) in
-            textField.placeholder = "Enter zipcode, city, or state"
-            tf = textField
-        }
-        
-        // add aciton
-        let okayAction = UIAlertAction(title: "Ok", style: .default) { (_) in
-            if let text = tf?.text, !text.isEmpty {
-                LocationGeocoder.getLocationData(from: text, completion: { (location, error) in
-                    if let _ = error {
-                        Alert.showLocationInputErrorAlert(in: self, inputString: "Unable to find location")
-                    }
-                    
-                    if let location = location {
-                        LocationLogic.sharedInstance.createLocation(isCurrentLocation: false, lat: location.coordinate.latitude, lon: location.coordinate.longitude)
-                    }
-                })
-            }
-        }
-        
-        // locate aciton
-        let currentLocationAction = UIAlertAction(title: "Use Current Location", style: .default) { (_) in
-            self.locationManager = CLLocationManager()
-            self.locationManager.delegate = self
-            self.locationManager.requestWhenInUseAuthorization()
-            if CLLocationManager.locationServicesEnabled() {
-                self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-                self.locationManager.startUpdatingLocation()
-            }
-        }
-        
-        // cancel action
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alertController.addAction(okayAction)
-        alertController.addAction(cancelAction)
-        alertController.addAction(currentLocationAction)
-        
-        vc.present(alertController, animated: true, completion: nil)
-    }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
@@ -217,7 +188,7 @@ extension LocationTableViewController {
         }
         locationManager.stopUpdatingLocation()
     }
-    
+
 }
 
 protocol LocationSelectDelegate {
